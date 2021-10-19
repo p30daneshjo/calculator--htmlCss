20b8ffece257be4c5f6fadbfa804570d296e99ba
JavadMahdizadeh <javad_m_ds@yahoo.com>
2021-10-19 10:11:47 +0330

    Ac+C

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 function data (number){
-    calc.display.value = number;
+    calc.display.value += number;
+}
+
+function ac(){
+    calc.display.value = "";
+}
+
+function c(){
+    calc.display.value = calc.display.value.slice( 0 , -1) ;
 }
